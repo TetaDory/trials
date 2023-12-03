f9bf6796d43a1bde074fe947670a0a788fd3fd99
TetaDory <113389069+TetaDory@users.noreply.github.com>
2023-12-03 16:28:11 +0000

    init

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import os
 
+database_url = os.environ.get('DATABASE_URL')
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 app.config['SESSION_PERMANENT'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'site.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 db = SQLAlchemy(app)
 
